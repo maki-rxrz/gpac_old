maki-rxrz/gpac_old

commit 37e1d7d3d8ebd325c1bd84f85ea284424388a974

Fix installtion problem in some particular situations.
* Don't add -s flag to install program to avoid hardcoded 'strip' to be
  invoked. It may not work in case of cross-compile across different arch/OS.
  Patch by Yasuhiro Ikeda <wipple625@gmail.com> and me.

* For mingw target, libgpac.dll is installed to bindir when 'make install' is specified.
  Installing to libdir is performed only when 'make install-lib' is specified.
  This change is for consistency with other platforms.

* Fix install/uninstall under mingw due to executable suffix.

* Fix inconsistent usages of DESTDIR and prefix.

Makefile
@@ -3,6 +3,12 @@
 #
 include config.mak
 
+ifeq ($(CONFIG_WIN32),yes)
+EXE=.exe
+else
+EXE=
+endif
+
 vpath %.c $(SRC_PATH)
 
 all:	version
@@ -42,7 +48,7 @@ distclean:
 	$(MAKE) -C src distclean
 	$(MAKE) -C applications distclean
 	$(MAKE) -C modules distclean
-	rm -f config.mak config.h
+	rm -f config.mak config.h include/gpac/revision.h
 
 dep:	depend
 
@@ -58,10 +64,16 @@ install:
 	$(INSTALL) -d "$(DESTDIR)$(prefix)/$(libdir)"
 	$(INSTALL) -d "$(DESTDIR)$(prefix)/bin"
 ifeq ($(DISABLE_ISOFF), no)
-	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/MP4Box "$(DESTDIR)$(prefix)/bin"
+	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/MP4Box$(EXE) "$(DESTDIR)$(prefix)/bin"
+ifeq ($(STRIPINSTALL),yes)
+	$(STRIP) $(DESTDIR)$(prefix)/bin/MP4Box$(EXE)
+endif
 endif
 ifeq ($(DISABLE_PLAYER), no)
-	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/MP4Client "$(DESTDIR)$(prefix)/bin"
+	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/MP4Client$(EXE) "$(DESTDIR)$(prefix)/bin"
+ifeq ($(STRIPINSTALL),yes)
+	$(STRIP) $(DESTDIR)$(prefix)/bin/MP4Client$(EXE)
+endif
 endif
 	if [ -d  $(DESTDIR)$(prefix)/$(libdir)/pkgconfig ] ; then \
 	$(INSTALL) $(INSTFLAGS) -m 644 gpac.pc "$(DESTDIR)$(prefix)/$(libdir)/pkgconfig" ; \
@@ -70,6 +82,11 @@ endif
 	$(INSTALL) bin/gcc/*.$(DYN_LIB_SUFFIX) "$(DESTDIR)$(moddir)"
 	rm -f $(DESTDIR)$(moddir)/libgpac.$(DYN_LIB_SUFFIX)
 	rm -f $(DESTDIR)$(moddir)/nposmozilla.$(DYN_LIB_SUFFIX)
+ifeq ($(STRIPINSTALL),yes)
+	if [ -f  $(DESTDIR)$(moddir)/*.$(DYN_LIB_SUFFIX) ] ; then \
+	$(STRIP) $(DESTDIR)$(moddir)/*.$(DYN_LIB_SUFFIX) ; \
+	fi
+endif
 	$(MAKE) installdylib
 	$(INSTALL) -d "$(DESTDIR)$(mandir)"
 	$(INSTALL) -d "$(DESTDIR)$(mandir)/man1";
@@ -97,26 +114,34 @@ uninstall:
 	rm -rf $(DESTDIR)$(moddir)
 	rm -rf $(DESTDIR)$(prefix)/$(libdir)/libgpac*
 	rm -rf $(DESTDIR)$(prefix)/$(libdir)/pkgconfig/gpac.pc
-	rm -rf $(DESTDIR)$(prefix)/bin/MP4Box
-	rm -rf $(DESTDIR)$(prefix)/bin/MP4Client
+	rm -rf $(DESTDIR)$(prefix)/bin/MP4Box$(EXE)
+	rm -rf $(DESTDIR)$(prefix)/bin/MP4Client$(EXE)
 	rm -rf $(DESTDIR)$(mandir)/man1/mp4box.1
 	rm -rf $(DESTDIR)$(mandir)/man1/mp4client.1
 	rm -rf $(DESTDIR)$(mandir)/man1/gpac.1
 	rm -rf $(DESTDIR)$(prefix)/share/gpac
 	rm -rf $(DESTDIR)$(prefix)/include/gpac
+	$(MAKE) uninstalldylib
 
 installdylib:
 ifeq ($(CONFIG_WIN32),yes)
-	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/libgpac.dll $(prefix)/$(libdir)
-else
-ifeq ($(DEBUGBUILD),no)
-	$(STRIP) bin/gcc/libgpac.$(DYN_LIB_SUFFIX)
+	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/libgpac.$(DYN_LIB_SUFFIX) $(DESTDIR)$(prefix)/bin
+ifeq ($(STRIPINSTALL),yes)
+	$(STRIP) $(DESTDIR)$(prefix)/bin/libgpac.$(DYN_LIB_SUFFIX)
 endif
+	cp -p $(DESTDIR)$(prefix)/bin/libgpac.$(DYN_LIB_SUFFIX) "$(DESTDIR)$(prefix)/$(libdir)"
+else
 ifeq ($(CONFIG_DARWIN),yes)
 	$(INSTALL) -m 755 bin/gcc/libgpac.$(DYN_LIB_SUFFIX) $(DESTDIR)$(prefix)/$(libdir)/libgpac.$(VERSION).$(DYN_LIB_SUFFIX)
+ifeq ($(STRIPINSTALL),yes)
+	$(STRIP) $(DESTDIR)$(prefix)/$(libdir)/libgpac.$(VERSION).$(DYN_LIB_SUFFIX)
+endif
 	ln -sf libgpac.$(VERSION).$(DYN_LIB_SUFFIX) $(DESTDIR)$(prefix)/$(libdir)/libgpac.$(DYN_LIB_SUFFIX)
 else
 	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/libgpac.$(DYN_LIB_SUFFIX).$(VERSION_SONAME) $(DESTDIR)$(prefix)/$(libdir)/libgpac.$(DYN_LIB_SUFFIX).$(VERSION_SONAME)
+ifeq ($(STRIPINSTALL),yes)
+	$(STRIP) $(DESTDIR)$(prefix)/$(libdir)/libgpac.$(DYN_LIB_SUFFIX).$(VERSION_SONAME)
+endif
 	ln -sf libgpac.$(DYN_LIB_SUFFIX).$(VERSION_SONAME) $(DESTDIR)$(prefix)/$(libdir)/libgpac.so.$(VERSION_MAJOR)
 	ln -sf libgpac.$(DYN_LIB_SUFFIX).$(VERSION_SONAME) $(DESTDIR)$(prefix)/$(libdir)/libgpac.so
 ifeq ($(DESTDIR)$(prefix),$(prefix))
@@ -127,7 +152,8 @@ endif
 
 uninstalldylib:
 ifeq ($(CONFIG_WIN32),yes)
-	rm -f $(prefix)/$(libdir)/libgpac.dll
+	rm -f $(DESTDIR)$(prefix)/bin/libgpac.$(DYN_LIB_SUFFIX)
+	rm -f $(DESTDIR)$(prefix)/$(libdir)/libgpac.$(DYN_LIB_SUFFIX)
 else
 ifeq ($(CONFIG_DARWIN),yes)
 	rm -f $(DESTDIR)$(prefix)/$(libdir)/libgpac.$(DYN_LIB_SUFFIX)
@@ -158,11 +184,11 @@ endif
 	$(MAKE) installdylib
 
 uninstall-lib:
-	rm -rf "$(prefix)/include/gpac/internal"
-	rm -rf "$(prefix)/include/gpac/modules"
-	rm -rf "$(prefix)/include/gpac/enst"
-	rm -rf "$(prefix)/include/gpac"
-	rm -rf "$(prefix)/$(libdir)/libgpac_static.a"
+	rm -rf "$(DESTDIR)$(prefix)/include/gpac/internal"
+	rm -rf "$(DESTDIR)$(prefix)/include/gpac/modules"
+	rm -rf "$(DESTDIR)$(prefix)/include/gpac/enst"
+	rm -rf "$(DESTDIR)$(prefix)/include/gpac"
+	rm -rf "$(DESTDIR)$(prefix)/$(libdir)/libgpac_static.a"
 	$(MAKE) uninstalldylib
 
 ifeq ($(CONFIG_DARWIN),yes)

configure
@@ -113,6 +113,7 @@ has_openjpeg="no"
 gprof_build="no"
 static_build="no"
 want_pic="no"
+enable_strip="no"
 has_joystick="no"
 has_xul="no"
 enable_joystick="no"
@@ -1705,7 +1706,7 @@ for opt do
         ### General build options
         --enable-gprof) gprof_build="yes";
             ;;
-        --strip) INSTFLAGS="-s $INSTFLAGS"
+        --strip) enable_strip="yes"
             ;;
         --std-allocator) use_std_alloc="yes"
             ;;
@@ -2868,6 +2869,7 @@ echo "CONFIG_AMR_WB_FT=$has_amr_wb" >> config.mak
 echo "DEBUGBUILD=$debuginfo" >> config.mak
 echo "GPROFBUILD=$gprof_build" >> config.mak
 echo "STATICBUILD=$static_build" >> config.mak
+echo "STRIPINSTALL=$enable_strip" >> config.mak
 
 echo "CONFIG_IPV6=$has_ipv6" >> config.mak
 if test "$has_ipv6" = "yes" ; then

commit 0d2e003a808b1338844ea5383853ccc97a647f8c

Fix a mistake in installation processing
Regression in 37e1d7d3d8ebd325c1bd84f85ea284424388a974 .
Customized by: Masaki Tanaka <maki.rxrz@gmail.com>

This bug is my responsibility. Sorry.

Makefile
@@ -125,6 +125,7 @@ uninstall:
 
 installdylib:
 ifeq ($(CONFIG_WIN32),yes)
+	$(INSTALL) -d "$(DESTDIR)$(prefix)/bin"
 	$(INSTALL) $(INSTFLAGS) -m 755 bin/gcc/libgpac.$(DYN_LIB_SUFFIX) $(DESTDIR)$(prefix)/bin
 ifeq ($(STRIPINSTALL),yes)
 	$(STRIP) $(DESTDIR)$(prefix)/bin/libgpac.$(DYN_LIB_SUFFIX)