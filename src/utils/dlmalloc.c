golgol7777/gpac

Fix some warnings and typo.

src/utils/dlmalloc.c
@@ -1292,7 +1292,7 @@ int mspace_mallopt(int, int);
 
 /*------------------------------ internal #includes ---------------------- */
 
-#ifdef WIN32
+#if defined(WIN32) && defined(MSC_VER)
 #pragma warning( disable : 4146 ) /* no "unsigned" warnings */
 #endif /* WIN32 */
 

maki-rxrz/gpac_old

src/utils/dlmalloc.c
@@ -1292,7 +1292,7 @@ int mspace_mallopt(int, int);
 
 /*------------------------------ internal #includes ---------------------- */
 
-#ifdef WIN32
+#if defined(WIN32) && defined(_MSC_VER)
 #pragma warning( disable : 4146 ) /* no "unsigned" warnings */
 #endif /* WIN32 */
 