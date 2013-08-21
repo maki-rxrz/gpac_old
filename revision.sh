3b5dce9e5daccf2d03b6dcb34aed997af646f6e8
Masaki Tanaka <maki.rxrz@gmail.com>
2013-08-21 21:41:45 +0900

    Extend the calculation of diff numbers

--- revision.sh
+++ revision.sh
@@ -9,32 +9,49 @@ git_revision=""
 git_rev_hash=""
 git_version=""
 
-if [ -d ".git" ]; then
- if which git >/dev/null
- then
-    base_branch=""
-    remote_branches="origin/plain svn/trunk"
-    local_branch="plain"
+chk_exist_branch () {
+    exist_branch=""
+    remote_branches="$2"
+    local_branch="$1"
     chk_exist="`git branch | grep $local_branch`"
     if test "$chk_exist" = "" ; then
         for branch in $remote_branches ; do
             chk_exist="`git branch -r | grep $branch`"
             if test "$chk_exist" != "" ; then
-                base_branch="remotes/$branch"
+                exist_branch="remotes/$branch"
                 break
             fi
         done
     else
-        base_branch="$local_branch"
+        exist_branch="$local_branch"
     fi
+    echo "$exist_branch"
+}
+
+if [ -d ".git" ]; then
+ if which git >/dev/null
+ then
+    base_branch=$(chk_exist_branch "plain" "origin/plain svn/trunk")
     if test "$base_branch" != "" ; then
         svn_revision="`git log -1 $base_branch | grep git-svn-id: | sed -e 's/^.*@//' -e 's/ .*//'`"
         git_version="$svn_revision"
+        # Check svn branch
         git_svn_revision="`git rev-list $base_branch | wc -l`"
+        # Check master branch
+        master_branch=$(chk_exist_branch "master" "origin/master")
+        git_master_revision="`git rev-list $master_branch | wc -l`"
+        # Check HEAD
         git_revision="`git rev-list HEAD | wc -l`"
+        # Calculate diff numbers
         git_rev_diff="$(($git_revision-$git_svn_revision))"
         if [ $git_rev_diff != 0 ] ; then
-            git_version="$git_version+$git_rev_diff"
+            if [ $git_revision != $git_master_revision ] ; then
+                git_rev_diff2="$(($git_revision-$git_master_revision))"
+                git_rev_diff1="$(($git_rev_diff-$git_rev_diff2))"
+                git_version="$git_version+$git_rev_diff1+$git_rev_diff2"
+            else
+                git_version="$git_version+$git_rev_diff"
+            fi
         fi
         if git status | grep -q "modified:" ; then
             git_version="${git_version}M"
