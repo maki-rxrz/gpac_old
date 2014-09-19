maki-rxrz/gpac_old

commit 633d1dc3085bdd2de56ba748a623d8a0b42fa543

updating M4A channel configuration to the latest 23001-8 FDIS (we support more values from other specs but I expect this one to be updated soon accordingly)
git-svn-id: svn://svn.code.sf.net/p/gpac/code/trunk/gpac@5401 63c20433-aa62-49bd-875c-5a186b69a8fb

include/gpac/avparse.h
@@ -174,9 +174,10 @@ static const u32 GF_M4ASampleRates[] =
 	16000, 12000, 11025, 8000, 7350, 0, 0, 0
 };
 
+/*new values should now be defined in 23001-8*/
 static const u32 GF_M4ANumChannels[] =
 {
-	1, 2, 3, 4, 5, 6, 8, 3, 4, 7, 8, 24, 8, 12, 10, 12, 14
+	1, 2, 3, 4, 5, 6, 8, 2, 3, 4, 7, 8, 24, 8, 12, 10, 12, 14
 };
 
 /*get Audio type from dsi. return audio codec type:*/

src/media_tools/av_parsers.c
@@ -1108,7 +1108,14 @@ GF_Err gf_m4a_parse_config(GF_BitStream *bs, GF_M4ADecSpecInfo *cfg, Bool size_k
 	} else {
 		cfg->base_sr = GF_M4ASampleRates[cfg->base_sr_index];
 	}
-	cfg->nb_chan = GF_M4ANumChannels[gf_bs_read_int(bs, 4)];
+	{
+		u32 nb_chan = gf_bs_read_int(bs, 4);
+		if (nb_chan == 0) {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CODING, ("[M4A config] invalid value 0, setting default 2 instead.\n"));
+			nb_chan = 2;
+		}
+		cfg->nb_chan = GF_M4ANumChannels[nb_chan-1];
+	}
 
 	if (cfg->base_object_type==5 || cfg->base_object_type==29) {
 		if (cfg->base_object_type==29) {

commit 4025ad2558a3aeccc97a41b15db914fdb6b211fe

Fixed aac channels parsing bug introduced in r5401
git-svn-id: svn://svn.code.sf.net/p/gpac/code/trunk/gpac@5428 63c20433-aa62-49bd-875c-5a186b69a8fb

include/gpac/avparse.h
@@ -180,10 +180,13 @@ static const u32 GF_M4ANumChannels[] =
 	1, 2, 3, 4, 5, 6, 8, 2, 3, 4, 7, 8, 24, 8, 12, 10, 12, 14
 };
 
+/*returns channel config value (as written in AAC DSI) for the given number of channels*/
+u32 gf_m4a_get_channel_cfg(u32 nb_chan);
+
 /*get Audio type from dsi. return audio codec type:*/
 typedef struct
 {
-	u32 nb_chan;
+	u32 nb_channels;
 	u32 base_object_type, base_sr, base_sr_index;
 	/*SBR*/
 	Bool has_sbr;

src/export.cpp
@@ -1330,6 +1330,7 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_mp3_get_next_header) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_mp3_get_next_header_mem) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_m4a_get_config) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_m4a_get_channel_cfg) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_m4a_write_config) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_m4a_write_config_bs) )
 

src/ietf/rtp_depacketizer.c
@@ -1223,7 +1223,7 @@ static GF_Err gf_rtp_payt_setup(GF_RTPDepacketizer *rtp, GF_RTPMap *map, GF_SDPM
 #ifndef GPAC_DISABLE_AV_PARSERS
 	case GF_RTP_PAYT_LATM:
 	{
-		u32 AudioMuxVersion, AllStreamsSameTime, numSubFrames, numPrograms, numLayers;
+		u32 AudioMuxVersion, AllStreamsSameTime, numSubFrames, numPrograms, numLayers, ch_cfg;
 		GF_M4ADecSpecInfo cfg;
 		char *latm_dsi = rtp->sl_map.config;
 		GF_BitStream *bs = gf_bs_new(latm_dsi, rtp->sl_map.configSize, GF_BITSTREAM_READ);
@@ -1245,7 +1245,7 @@ static GF_Err gf_rtp_payt_setup(GF_RTPDepacketizer *rtp, GF_RTPMap *map, GF_SDPM
 		} else {
 			cfg.base_sr = GF_M4ASampleRates[cfg.base_sr_index];
 		}
-		cfg.nb_chan = gf_bs_read_int(bs, 4);
+		ch_cfg = gf_bs_read_int(bs, 4);
 		if (cfg.base_object_type==5 || cfg.base_object_type==29) {
 			if (cfg.base_object_type==29) {
 				cfg.has_ps = 1;
@@ -1266,7 +1266,8 @@ static GF_Err gf_rtp_payt_setup(GF_RTPDepacketizer *rtp, GF_RTPMap *map, GF_SDPM
 		/*write as regular AAC*/
 		gf_bs_write_int(bs, cfg.base_object_type, 5);
 		gf_bs_write_int(bs, cfg.base_sr_index, 4);
-		gf_bs_write_int(bs, cfg.nb_chan, 4);
+
+		gf_bs_write_int(bs, ch_cfg, 4);
 		gf_bs_align(bs);
 		gf_bs_get_content(bs, &rtp->sl_map.config, &rtp->sl_map.configSize);
 		gf_bs_del(bs);

src/media_tools/av_parsers.c
@@ -1254,6 +1254,16 @@ u32 gf_latm_get_value(GF_BitStream *bs)
 }
 
 GF_EXPORT
+u32 gf_m4a_get_channel_cfg(u32 nb_chan)
+{
+	u32 i, count = sizeof(GF_M4ANumChannels)/sizeof(u32);
+	for (i=0; i<count; i++) {
+		if (GF_M4ANumChannels[i] == nb_chan) return i+1;
+	}
+	return 0;
+}
+
+GF_EXPORT
 GF_Err gf_m4a_write_config_bs(GF_BitStream *bs, GF_M4ADecSpecInfo *cfg)
 {
 	if (!cfg->base_sr_index) {
@@ -1285,7 +1295,7 @@ GF_Err gf_m4a_write_config_bs(GF_BitStream *bs, GF_M4ADecSpecInfo *cfg)
 	if (cfg->nb_chan == 8) {
 		gf_bs_write_int(bs, 7, 4);
 	} else {
-		gf_bs_write_int(bs, cfg->nb_chan, 4);
+		gf_bs_write_int(bs, gf_m4a_get_channel_cfg( cfg->nb_chan) , 4);
 	}
 
 	if (cfg->base_object_type==5 || cfg->base_object_type==29) {

src/media_tools/media_import.c
@@ -965,7 +965,7 @@ GF_Err gf_import_aac_adts(GF_MediaImporter *import)
 	memset(&acfg, 0, sizeof(GF_M4ADecSpecInfo));
 	acfg.base_object_type = hdr.profile;
 	acfg.base_sr = sr;
-	acfg.nb_chan = hdr.nb_ch;
+	acfg.nb_chan = gf_m4a_get_channel_cfg(hdr.nb_ch);
 	acfg.sbr_object_type = 0;
 	if (import->flags & GF_IMPORT_SBR_EXPLICIT) {
 		acfg.has_sbr = 1;